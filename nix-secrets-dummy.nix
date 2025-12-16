11703c521d6d556d6a57e8bedc0be84c0d4b2c1b
Misaka13514 <Misaka13514@gmail.com>
2025-12-16 15:58:42 +0800

    remove caddy

--- nix-secrets-dummy.nix
+++ nix-secrets-dummy.nix
@@ -9,6 +9,7 @@
         authorizedKeys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK7DuYiwiKT6VCfWLCE/OTALtgqujaZWWoco9pKmGKGP openpgp:0x88613226"
         ];
+        homeDomain = "example.org";
       };
     };
 }
--- nixosConfigurations/sakanaa.nix
+++ nixosConfigurations/sakanaa.nix
@@ -6,61 +6,39 @@
   nixosModules,
   homeModules,
   secretsPath,
+  nixSecrets,
   ...
 }:
 let
-  cloudflareSecrets = [
-    "cloudflare-api-token"
-    "cloudflare-account-id"
-    "cloudflare-r2-access-key"
-    "cloudflare-r2-secret-key"
-  ];
+  domain = nixSecrets.homeDomain;
 in
 {
   imports = with nixosModules; [
     roles.container
     users.atri
-    services.caddy
   ];
 
-  sops.secrets = lib.genAttrs cloudflareSecrets (name: {
+  sops.secrets."cloudflare-api-token" = {
     format = "yaml";
     sopsFile = "${secretsPath}/cloudflare.yaml";
-    key = name;
-  });
+  };
 
-  sops.templates."caddy-env" = {
+  sops.templates."acme-credentials" = {
     content = ''
-      CLOUDFLARE_API_TOKEN=${config.sops.placeholder."cloudflare-api-token"}
-      CLOUDFLARE_ACCOUNT_ID=${config.sops.placeholder."cloudflare-account-id"}
-      CLOUDFLARE_R2_ACCESS_KEY=${config.sops.placeholder."cloudflare-r2-access-key"}
-      CLOUDFLARE_R2_SECRET_KEY=${config.sops.placeholder."cloudflare-r2-secret-key"}
-    '';
-    owner = "caddy";
-    restartUnits = [ "caddy.service" ];
-  };
-
-  services.caddy = {
-    logFormat = ''
-      level INFO
-    '';
-    environmentFile = config.sops.templates."caddy-env".path;
-    globalConfig = ''
-      admin unix//run/caddy/admin.sock
-      dns cloudflare {$CLOUDFLARE_API_TOKEN}
-      acme_ca https://acme-v02.api.letsencrypt.org/directory
-      auto_https disable_redirects
-      storage s3 {
-        host       "{$CLOUDFLARE_ACCOUNT_ID}.r2.cloudflarestorage.com"
-        bucket     "certmagic-s3"
-        access_id  "{$CLOUDFLARE_R2_ACCESS_KEY}"
-        secret_key "{$CLOUDFLARE_R2_SECRET_KEY}"
-      }
+      CF_DNS_API_TOKEN=${config.sops.placeholder."cloudflare-api-token"}
     '';
   };
 
-  systemd.services.caddy.serviceConfig = {
-    RuntimeDirectory = "caddy";
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "acme@apeiria.net";
+    certs = {
+      "${domain}" = {
+        extraDomainNames = [ "*.${domain}" ];
+        dnsProvider = "cloudflare";
+        environmentFile = config.sops.templates."acme-credentials".path;
+      };
+    };
   };
 
   noa.nix.enableMirrorSubstituter = true;
