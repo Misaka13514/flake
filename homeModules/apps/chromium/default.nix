24c4015e3aef408ecf43bf0aed15b8111c0bfac7
Misaka13514 <Misaka13514@gmail.com>
2025-12-21 15:57:39 +0800

    refactor chromium

--- homeModules/apps/chromium/default.nix
+++ homeModules/apps/chromium/default.nix
@@ -10,16 +10,7 @@ in
 {
   programs.chromium = {
     enable = true;
-    package = pkgs.symlinkJoin {
-      name = "chromium";
-      paths = [ pkgs.chromium ];
-      buildInputs = [ pkgs.makeWrapper ];
-      postBuild = ''
-        wrapProgram $out/bin/chromium \
-          --set GOOGLE_DEFAULT_CLIENT_ID 77185425430.apps.googleusercontent.com \
-          --set GOOGLE_DEFAULT_CLIENT_SECRET OTJgUOQcT7lO7GsGZq2G4IlT
-      '';
-    };
+    package = pkgs.flakePackages.chromium;
     extensions = [
       { id = "ddkjiahejlhfcafbddmgiahcphecmpfh"; } # uBlock Origin Lite
       { id = "mnjggcdmjocbbbhaepdhchncahnbgone"; } # SponsorBlock
---
+++ packages/chromium.nix
@@ -0,0 +1,17 @@
+{
+  pkgs,
+  clientId ? "77185425430.apps.googleusercontent.com",
+  clientSecret ? "OTJgUOQcT7lO7GsGZq2G4IlT",
+}:
+
+pkgs.symlinkJoin {
+  name = "chromium";
+  paths = [ pkgs.chromium ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    wrapProgram $out/bin/chromium \
+      --set GOOGLE_DEFAULT_CLIENT_ID ${clientId} \
+      --set GOOGLE_DEFAULT_CLIENT_SECRET ${clientSecret}
+  '';
+  meta = pkgs.chromium.meta;
+}
