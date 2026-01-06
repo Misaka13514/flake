b30cfd9cef25ef908cd8e7888303020863b26ed3
Misaka13514 <Misaka13514@gmail.com>
2026-01-06 15:58:08 +0800

    add nerd fonts

--- nixosModules/roles/desktop/fonts/default.nix
+++ nixosModules/roles/desktop/fonts/default.nix
@@ -9,6 +9,9 @@
       noto-fonts-cjk-serif
       source-han-sans
       source-han-serif
+
+      nerd-fonts.fira-code
+      nerd-fonts.symbols-only
     ];
 
     enableGhostscriptFonts = true;
@@ -17,12 +20,18 @@
     fontconfig.defaultFonts = {
       serif = lib.mkAfter [
         "Noto Serif CJK SC"
+        "Symbols Nerd Font"
+        "Noto Color Emoji"
       ];
       sansSerif = lib.mkAfter [
         "Noto Sans CJK SC"
+        "Symbols Nerd Font"
+        "Noto Color Emoji"
       ];
       monospace = lib.mkAfter [
         "Noto Sans Mono CJK SC"
+        "Symbols Nerd Font Mono"
+        "Noto Color Emoji"
       ];
       emoji = [ "Noto Color Emoji" ];
     };
