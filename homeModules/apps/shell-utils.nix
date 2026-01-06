1cab91eaa168c23eeb4e52b15098f0f14ba131a4
Misaka13514 <Misaka13514@gmail.com>
2026-01-06 15:52:34 +0800

    add modern unix shell

--- homeModules/apps/shell-utils.nix
+++ homeModules/apps/shell-utils.nix
@@ -6,6 +6,7 @@
 }:
 let
   extraPackages = with pkgs; [
+    any-nix-shell
     fastfetch
     ldns
     nmap
@@ -13,6 +14,8 @@ let
     pciutils
     usbutils
     whois
+    dust
+    duf
   ];
 in
 {
@@ -30,7 +33,38 @@ in
     };
   };
 
+  programs.starship = {
+    enable = true;
+    enableFishIntegration = true;
+    enableTransience = true;
+  };
+
   programs.fish = {
+    enable = true;
+    plugins = map (x: { inherit (x) name src; }) (
+      with pkgs.fishPlugins;
+      [
+        plugin-git
+        fzf-fish
+        puffer
+      ]
+    );
+    shellInit = "set -g fish_greeting";
+    interactiveShellInit = ''
+      any-nix-shell fish --info-right | source
+    '';
+    shellAliases = {
+      ".." = "cd ../";
+      "n" = "nvim";
+      "ls" = "eza -l";
+      "l" = "eza -l";
+      "ll" = "eza -al";
+      "tree" = "eza --tree";
+      "gg" = "lazygit";
+    };
+  };
+
+  programs.lazygit = {
     enable = true;
   };
 
@@ -41,4 +75,38 @@ in
     enable = true;
     enableFishIntegration = true;
   };
+
+  # Modern unix series
+  programs.eza = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.zoxide = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+
+  programs.bat.enable = true;
+
+  programs.btop.enable = true;
+
+  programs.jq.enable = true;
+
+  programs.ripgrep.enable = true;
+
+  programs.fd = {
+    enable = true;
+    ignores = [
+      ".git/"
+      "node_modules/"
+    ];
+  };
+
+  programs.tealdeer.enable = true;
 }
