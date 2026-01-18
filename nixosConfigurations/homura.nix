9a761f3896b8db21d0ab23527fafe21129c4211f
Misaka13514 <Misaka13514@gmail.com>
2026-01-18 16:49:36 +0800

    homura: add headscale

--- nixosConfigurations/homura.nix
+++ nixosConfigurations/homura.nix
@@ -123,6 +123,22 @@ in
     };
   };
 
+  services.headscale = {
+    enable = true;
+    address = "127.0.0.1";
+    port = 40180;
+    settings = {
+      server_url = "https://${nixSecrets.headscaleDomain}";
+      dns = {
+        magic_dns = true;
+        base_domain = "paths.${domain}";
+        override_local_dns = false;
+      };
+      prefixes.allocation = "random";
+      derp.server.enable = false;
+    };
+  };
+
   users.users.nginx.extraGroups = [ "acme" ];
   services.nginx = {
     enable = true;
@@ -144,6 +160,21 @@ in
         extraConfig = ''
           return 444;
         '';
+      };
+
+      "${nixSecrets.headscaleDomain}" = {
+        useACMEHost = domain;
+        forceSSL = true;
+        locations."/robots.txt" = {
+          extraConfig = ''
+            add_header Content-Type text/plain;
+            return 200 "User-agent: *\nDisallow: /";
+          '';
+        };
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.headscale.port}";
+          proxyWebsockets = true;
+        };
       };
 
       "${nixSecrets.homuraDomain}" = {
--- nixosModules/services/docker.nix
+++ nixosModules/services/docker.nix
@@ -12,8 +12,34 @@
   config = {
     virtualisation.docker = {
       enable = true;
-      daemon.settings = lib.mkIf config.noa.docker.useRegistryMirror {
-        registry-mirrors = [ "https://docker.1ms.run" ];
+      daemon.settings = {
+        # ================= IP Address Management (IPAM) Strategy =================
+        # To prevent routing table conflicts with existing overlay networks,
+        # the following ranges MUST BE AVOIDED:
+        # - 10.0.0.0/8      -> Reserved for NeoNetwork.
+        # - 100.64.0.0/10   -> Reserved for Tailscale/Headscale (CGNAT).
+        # - 172.20.0.0/14   -> Reserved for DN42.
+        # - 192.168.0.0/16  -> Avoided to prevent collision with local Wi-Fi/LAN.
+        # =========================================================================
+
+        # Default Bridge (docker0)
+        # Sized to /24 (254 IPs). Sufficient for standalone 'docker run' containers.
+        "bip" = "172.26.0.1/24";
+
+        # Dynamic Network Pools (used by Docker Compose)
+        # Base: 172.26.16.0/20 covers 172.26.16.0 to 172.26.31.255 (4096 IPs).
+        # Size: 27 means each Compose project gets a /27 subnet (32 IPs).
+        # Capacity: This allows for exactly 128 distinct Docker Compose networks.
+        "default-address-pools" = [
+          {
+            "base" = "172.26.16.0/20";
+            "size" = 27;
+          }
+        ];
+
+        "registry-mirrors" = lib.mkIf config.noa.docker.useRegistryMirror [
+          "https://docker.1ms.run"
+        ];
       };
     };
     virtualisation.oci-containers.backend = "docker";
