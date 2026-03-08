9810b71930f798a6f07f3eb5d8059df30b4611bc
Misaka13514 <Misaka13514@gmail.com>
2026-03-08 14:17:59 +0800

    midori: hardware tweak

--- hardwares/midori.nix
+++ hardwares/midori.nix
@@ -9,4 +9,20 @@
 
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
+
+  hardware.enableRedistributableFirmware = true;
+
+  boot.kernelParams = [
+    "console=tty1"
+    "console=ttyS0,115200n8"
+  ];
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="drm", KERNEL=="card*", KERNELS=="0000:00:02.0", SYMLINK+="dri/pve-igpu"
+    SUBSYSTEM=="drm", KERNEL=="card*", KERNELS=="0000:00:01.0", SYMLINK+="dri/pve-virtgpu"
+  '';
+
+  environment.sessionVariables = {
+    KWIN_DRM_DEVICES = "/dev/dri/pve-igpu:/dev/dri/pve-virtgpu";
+  };
 }
